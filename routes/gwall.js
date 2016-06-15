d8d6f8fdcdefc399d35954b54d6cad245ae65d48
rayk216 <raykoren@gmail.com>
2016-06-14 21:07:05 -0600

    post are working

--- routes/gwall.js
+++ routes/gwall.js
@@ -1,19 +1,34 @@
 var express = require('express');
 var router = express.Router();
-
+var knex = require('../db/knex.js');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('gwall');
+  knex.table('posts').select().then(function(posts){
+    res.render('gwall', {posts: posts});
+
+  });
 });
-router.get('/add', function(req, res, next) {
-  res.render('add');
+router.get('/post', function(req, res, next) {
+  res.render('post');
 });
-router.get('/add', function(req, res, next) {
-  res.render('gwall');
+router.post('/post', function(request, response, next) {
+  var username = request.body.username;
+  var image = request.body.image;
+  var rant = request.body.rant;
+  var data = {
+    username: username,
+    image: image,
+    rant: rant
+  };
+  // insert into tables
+  knex.table('posts').insert(data).then(function(){
+    console.log('you did it!');
+    response.redirect('/gwall');
+  }).catch(function(error){
+      next(error);
+  });
 });
-router.get('/add', function(req, res, next) {
-  res.render('gwall');
-});
+
 
 
 module.exports = router;
